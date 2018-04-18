03a531ffb5690382e73bd40682be0a0978b2b5ba
fourierhai <18251893986@163.com>
2018-04-18 15:20:50 +0800

    加入了消息中心

--- toutiao/src/main/java/com/nowcoder/dao/NewsDAO.java
+++ toutiao/src/main/java/com/nowcoder/dao/NewsDAO.java
@@ -17,6 +17,12 @@ public interface NewsDAO {
             ") values (#{title},#{link},#{image},#{likeCount},#{commentCount},#{createdDate},#{userId})"})
     int addNews(News news);
 
+    @Select({"select ", SELECT_FIELDS , " from ", TABLE_NAME, " where id=#{id}"})
+    News getById(int id);
+
+    @Update({"update ", TABLE_NAME, " set comment_count = #{commentCount} where id=#{id}"})
+    int updateCommentCount(@Param("id") int id, @Param("commentCount") int commentCount);
+
     List<News> selectByUserIdAndOffset(@Param("userId") int userId, @Param("offset") int offset,
                                        @Param("limit") int limit);
 }
